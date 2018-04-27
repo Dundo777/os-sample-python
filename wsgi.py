922c52188317a9faf0829fbe990d387147d30afc
Dundo777 <dundo.777@gmail.com>
2018-04-27 16:10:27 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from kafka import KafkaProducer
+>>> 
+>>> 
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
+    producer = KafkaProducer(bootstrap_servers='localhost:9092')
+    producer.send('test', 'hi!')
     return "OpenShift Hello World!"
 
 if __name__ == "__main__":
