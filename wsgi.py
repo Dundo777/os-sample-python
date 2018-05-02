50dbd6b21d9148c7c7a059803146780a817b38d2
Dundo777 <dundo.777@gmail.com>
2018-05-02 12:57:40 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -8,7 +8,7 @@ application = Flask(__name__)
 def hello():
     producer = KafkaProducer(bootstrap_servers='apache-kafka:9092')
     for i in range(100):
-        txt = "Hi " + str(i)
+        txt = "Hiii " + str(i)
         producer.send('test', bytes(txt.encode('ASCII')))
     producer.flush()
     return "OpenShift Hello World!"
